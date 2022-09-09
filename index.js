2168dbf64433d41c876fc1b40f242bf862cdf251
QiuLiang Wang <laoqiu@QiuLiangdeMacBook-Pro.local>
2022-09-09 18:55:10 +0800

    fix if alpha is fully transparent, use the pixel from the prev frame

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 "use strict"
 
 const { GifReader } = require("omggif")
-const arrayRange = require("array-range")
+// const ndarray = require("ndarray")
+
+const TAG = "decodeGIF"
 
 module.exports = data => {
 	const reader = new GifReader(data)
 
 	let currentTimeCode = 0
-	const frames = arrayRange(0, reader.numFrames()).map(frameIndex => {
+	let frames = []
+
+	console.log(TAG, "reader.numFrames()", reader.numFrames())
+
+	for (let frameIndex = 0; frameIndex < reader.numFrames(); frameIndex++) {
 		const { delay } = reader.frameInfo(frameIndex)
 
 		const frameData = new Uint8ClampedArray(reader.width * reader.height * 4)
 		reader.decodeAndBlitFrameRGBA(frameIndex, frameData)
+
+		if (frameIndex > 0) {
+			const prevFrameData = frames[frameIndex - 1].data
+			// console.log(TAG, "prevFrameData", prevFrameData)
+			for (let i = 0; i < frameData.length; i += 4) {
+				if (frameData[i] === 0 && frameData[i + 1] === 0 && frameData[i + 2] === 0) {
+					frameData[i] = prevFrameData[i]
+					frameData[i + 1] = prevFrameData[i + 1]
+					frameData[i + 2] = prevFrameData[i + 2]
+					frameData[i + 3] = prevFrameData[i + 3]
+				}
+			}
+		}
+
+		// console.log(TAG, frameData)
 
 		const data = {
 			timeCode: currentTimeCode,
@@ -20,8 +41,9 @@ module.exports = data => {
 
 		currentTimeCode += delay * 10
 
-		return data
-	})
+		frames.push(data)
+	}
+
 
 	return {
 		width: reader.width,
